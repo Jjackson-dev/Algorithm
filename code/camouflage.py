3b4fb27a1a63b5e06de283705cea0944adbc745d
Min-Jae Song <50691076+hsu-201458085@users.noreply.github.com>
2021-01-31 21:07:12 +0900

    Create camouflage.py

---
+++ code/camouflage.py
@@ -0,0 +1,11 @@
+def solution(clothes):
+    dic = {}
+    for _ , cate in clothes:
+        if not cate in dic:
+            dic[cate] = 1
+        else:
+            dic[cate] += 1
+    result = 1
+    for i in dic.keys():
+        result *= dic[i] + 1
+    return result - 1
