623d1507126a7216c2b907b58c238fe8cef72d38
Min-Jae Song <50691076+hsu-201458085@users.noreply.github.com>
2021-02-12 21:02:43 +0900

    Update merge_sorted_list.py

--- code/merge_sorted_list.py
+++ code/merge_sorted_list.py
@@ -10,8 +10,19 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        if (not l1) or (l2 and l1.val > l2.val):
-            l1, l2 = l2, l1
+        # Swap l1 and l2
+        # if l2.val smaller than l1.val
+        # elif l1 is None but l2 is not None
+        # Do Not swaping when l2 is None
+        if not l1 or l2 and l1.val > l2.val:
+            l1, l2 = l2, l1 #swap l1 and l2
+        
+        # l1.next = recursion(l1.next, l2), if l1 is not None 
+        # In recursion function, while and if are same 
         if l1:
             l1.next = self.mergeTwoLists(l1.next, l2)
+        
+        #final l1 --> l1:None, l2:None --> Returned from None to 1, in reverse
         return l1
+            
+             
